bf7453f0e25062b0b7e7a7eab27f32390be95ed3
Ayaka Kume <kume@spiq.jp>
2015-05-08 18:34:32 -0400

    add codes

--- includes/fingertip.py
+++ includes/fingertip.py
@@ -1,9 +1,48 @@
-def find_fingertip(frame, label):
+import cv2
+import numpy as np
+import pdb
+import time
+import math
+def find_fingertip(label, mask):
     """
     Find the location of the fingertips. Only have to return one pixel. 
-    @param np.array         frame               a numpy array of size(480,640,3)
+    @param np.array         hand_mask               a numpy array of size(480,640,1)
     @label int              label               An interger represting the label of the frame. 
                                                 Look at posture.py for more informations. 
     return (int, int)       loc                 The location of the fingertip of the longest finger.
     """ 
-    pass
+    (x,y) = mask.nonzero()
+    # find the minimum / maximum x / y
+    x_min = min(x)
+    y_min = min(y)
+    x_max = max(x)
+    y_max = max(y)
+    # find the center
+    c_x = x.mean(axis=0)
+    c_y = y.mean(axis=0)
+    # find the most largest edge (= wrist)
+    up = (sum(mask[x_min,:])/255)
+    bottom = (sum(mask[x_max,:])/255)
+    left = (sum(mask[:,y_min])/255)
+    right = (sum(mask[:,y_max])/255)
+    wrist = max(up,bottom,left,right)
+    wrist_ch = ""
+    #print x_min,x_max,y_min,y_max
+    # mask wrist side
+    if wrist == up:
+        mask[:c_x,:] = 0
+        wrist_ch = "up"
+    elif wrist == bottom:
+        wrist_ch = "down"
+        mask[c_x:,:] = 0
+    elif wrist == left:
+        wrist_ch = "left"
+        mask[:,:c_y] = 0
+    elif wrist == right:
+        wrist_ch = "right"
+        mask[:,c_y:] = 0
+    # find the most furthest point (= finger)
+    (x,y) = mask.nonzero()
+    z = (x - c_x)**2 + (y - c_y)**2
+    return x[z.argmax(0)],y[z.argmax(0)],wrist_ch
+
--- includes/hand_detection.py
+++ includes/hand_detection.py
@@ -1,111 +1,26 @@
+"""Package for hand detection"""
 import cv2
 import numpy as np
 import pdb
 import time
-"""Package for hand detection"""
-def unique(a):
-    """ remove duplicate columns and rows
-        from http://stackoverflow.com/questions/8560440 """
-    order = np.lexsort(a.T)
-    a = a[order]
-    diff = np.diff(a, axis=0)
-    ui = np.ones(len(a), 'bool')
-    ui[1:] = (diff != 0).any(axis=1)
-    return a[ui]
+import math
 
-def skin_detection(image, haarcascades, flood_diff=7, min_face_size=(6,6),
-        num_iter=5, step=3):
-    # face detection
-    faces = haarcascades.detectMultiScale(image, minSize=min_face_size)
-    mask = np.zeros(image.shape)
+def hand_detection(image):
     hsv_im = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
-    if len(faces) >= 0:
-        for y in range(image.shape[0]):
-            for x in range(image.shape[1]):
-                r = image[y,x,2]
-                g = image[y,x,1]
-                b = image[y,x,0]
-                s = hsv_im[y,x,1]
-                v = hsv_im[y,x,2]
-                if r > 95 and g > 40 and b > 20 and max(r,g,b) - min(r,g,b) > 15 and abs(r-g) > 15 and r > g and r > b and s > 50:
-                        mask[y,x,:] = 255
-        if len(faces) > 0:
-            mask[faces[0][1]:faces[0][1]+faces[0][3],faces[0][0]:faces[0][0]+faces[0][2]] = 0
-    if len(faces) > 0:
-        mask2 = mask
-        # convert to hsv
-        image_original = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-        image2 = np.copy(image_original)
-        image2[image2==[0,0,0]] = 1
-
-        skin_color = np.zeros((len(faces), 3))
-        for i, face in enumerate(faces):
-            image_face = image2[face[1]:face[1]+face[3],face[0]:face[0]+face[2]]
-            skin_color[i] = np.array([image_face[image_face.shape[0]/2, image_face.shape[1]/2]])
-        mask = np.zeros(image_original.shape)
-        #### osoi!!!!
-        for i in range(num_iter):
-        # for each pixel, call floodFill(image2) if it is close to skin_color
-            for y in range(0, image2.shape[0], step):
-                for x in range(0, image2.shape[1], step):
-                    color = image2[y,x]
-                    if (color!=(0,0,0)).any():
-                        if any((np.abs(skin_color-color)<=(flood_diff,)*3).all(1)):
-                            cv2.floodFill(image2, None, (x,y), (0, 0, 0),
-                                loDiff=(flood_diff,)*3, upDiff=(flood_diff,)*3)
-
-            # update mask image and skin_color
-            mask[image2==(0,0,0)] = 255
-            skin_color = image_original[mask.nonzero()]
-            skin_color.shape = (skin_color.shape[0]/3, 3)
-            skin_color = unique(skin_color)
-        mask[face[1]:face[1]+face[3],face[0]:face[0]+face[2]] = 0
-        mask2[face[1]:face[1]+face[3],face[0]:face[0]+face[2]] = 0
-        if np.sum(mask) < 20000:
-            print 'tisai'
-            mask = mask2
-    k = np.ones((3,3),np.uint8)
-    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, k)
-    mask = cv2.resize(mask,(mask.shape[1]*4,mask.shape[0]*4))
+    HSV = cv2.split(hsv_im)
+    S = HSV[1]
+    V = HSV[2]
+    RGB = cv2.split(image)
+    Blue = RGB[0]
+    Green = RGB[1]
+    Red = RGB[2]
+    mask = Red - Blue
+    rg = Red - Green
+    mask[mask < 20] = 0
+    mask[rg < 20] = 0
+    mask[mask > 0] = 255
+    mask[Blue > Red] = 0
+    mask[Green > Red] = 0
+    mask = cv2.medianBlur(mask,5)
     mask = np.bool_(mask)
     return mask
-def grab(im):
-    im_s = cv2.resize(im,(im.shape[1]/4,im.shape[0]/4))
-    # rect
-    x2 = im_s.shape[1]*19/20
-    y2 = im_s.shape[0]#*4/5
-    x1 = 0 #im_s.shape[1]*1/40
-    y1 = im_s.shape[0]*5/16
-    # masks
-    mask = np.zeros(im_s.shape[:2],np.uint8)
-    bgd_model = np.zeros((1,65),np.float64)
-    fgd_model = np.zeros((1,65),np.float64)
-    rect = (x1,y1,x2,y2)
-    # grabcut
-    cv2.grabCut(im_s,mask,rect,bgd_model,fgd_model,3,cv2.GC_INIT_WITH_RECT)
-    mask2 = np.where((mask==2)|(mask==0),0,1).astype("uint8")
-    #mask2 = cv2.resize(mask2,(mask2.shape[1]*8,mask2.shape[0]*8))
-    im_s = im_s*mask2[:,:,np.newaxis]
-    return im_s
-def hand_detection(im):
-    #filename = "testdata/Image" + ("00"+str(i))[-2:] + ".bmp"
-    #print filename
-    #filename_out = (''.join(filename.split('.')[:-1])
-    #                + '_out_1_%d.' + filename.split('.')[-1])
-    # for face detection
-    hc = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
-    # load image
-    #im = cv2.imread(filename)
-    im2 = grab(im)
-    #cv2.imwrite(filename_out % 4, im2)
-    # Skin detection using face information
-    mask = skin_detection(im2, hc)
-    #mask = np.bool_(mask)
-    mask = np.bool_(mask)
-    th = mask[:,:,0]
-    # pdb.set_trace()
-    #cv2.imwrite(filename_out % 1, im)
-    #cv2.imwrite(filename_out % 2, np.uint8(mask)*255)
-    #im[mask==False] = 0
-    #cv2.imwrite(filename_out % 3, im)
-    return th
