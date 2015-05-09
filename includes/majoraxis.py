333fa0e7d5d3a9b650f44fe01cfa925fbd16d3e4
Ayaka Kume <kume@spiq.jp>
2015-05-09 00:22:16 -0400

    add major axis detection

--- includes/majoraxis.py
+++ includes/majoraxis.py
@@ -1,4 +1,4 @@
-def majoraxis(img):
+def majoraxis(mask):
     """
     Find the major axis of the hand. The input image is a binary image of the hand mask.
     Return the angle and the y-intercept of the major axis. 
@@ -7,6 +7,25 @@ def majoraxis(img):
 
 
     @param np.array img     The input image A numpy.array of size (480,640), dtype=np._bool
-    return (theta, b)       The majoraxis described with the angle theta and the y-intercept b. 
-                            Theta is measured in degrees. """
-    pass
+    return (theta, c_x, c_y) The majoraxis described with the angle theta and the center point of the object. 
+                            Theta is measured in radius. """
+    # non zero area
+    (x,y) = mask.nonzero()
+    # area
+    area = len(x)
+    # center position
+    c_x = x.mean(axis=0)
+    c_y = y.mean(axis=0)
+    # second moment
+    a_o = (np.multiply(x,x)).sum(axis=0)
+    b_o = (np.multiply(x,y)).sum(axis=0)
+    c_o = (np.multiply(y,y)).sum(axis=0)
+    # the minimum moment of inerita
+    # convert the second moment for the origin to the second moment for the center
+    a = a_o - area*(c_x**2)
+    b = 2*b_o - 2 * area * c_x * c_y
+    c = c_o - area*(c_y**2)
+    # find theta for the major axis
+    theta = math.atan2(b,a-c)/2.0
+    #print i,",",theta, ",",c_x,",",c_y
+    return theta, c_x, c_y
