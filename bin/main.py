f031731d0d573c299952a3416b2d3aa08bf0c3b3
du48s03 <u5b890402@gmail.com>
2015-05-11 13:39:17 -0400

    changed maint

--- bin/main.py
+++ bin/main.py
@@ -11,27 +11,34 @@ import fingertip
 def cutframe(frame):
     return frame[:380]
 
-modelfilename = sys.argv[1]
-pos_recognizer = posture.PostureRecognizer.load(modelfilename)
-ui = gui.GUI()
-#Get the image and do the classification here
-cap = cv2.VideoCapture(0)
+def main():
+    modelfilename = sys.argv[1]
+    pos_recognizer = posture.PostureRecognizer.load(modelfilename)
+    ui = gui.GUI()
+    #Get the image and do the classification here
+    cap = cv2.VideoCapture(0)
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    frame = cutframe(frame)
-    # Display the input stream only for debug purposes
-    cv2.imshow('input',frame)
+    while(True):
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+        frame = cutframe(frame)
+        # Display the input stream only for debug purposes
+        cv2.imshow('input',frame)
 
-    
-    label, hand_mask = pos_recognizer.classify(frame)
-    location, wrist_end = fingertip.find_fingertip(label, hand_mask)
-    touching = posture.isTouching(frame, label, location, wrist_end)
+        
+        label, hand_mask = pos_recognizer.classify(frame)
+        location, wrist_end = fingertip.find_fingertip(label, hand_mask)
+        touching = posture.isTouching(frame, label, location, wrist_end)
 
-    #=======The grammar goes here=============
-    ui.handle_input(label, location, touching)
+        #=======The grammar goes here=============
+        ui.handle_input(label, location, touching)
 
-    cv2.imshow('Canvas', ui.get_screen())
-    pressedKey = cv2.waitKey(60)
+        cv2.imshow('Canvas', ui.get_screen())
+        pressedKey = cv2.waitKey(60)
+        if pressedKey == ord('q'):
+            
 
+
+
+if __name__ == '__main__':
+    main()
